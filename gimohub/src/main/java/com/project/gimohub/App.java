7a4f71864fc6acf7710bd98eb6bd8a8a29ed4a45
Nadeeshan Gimhana <gimhanadesilva.15@cse.mrt.ac.lk>
2018-12-24 18:37:34 +0530

    use Cat as Object

--- gimohub/src/main/java/com/project/gimohub/App.java
+++ gimohub/src/main/java/com/project/gimohub/App.java
@@ -9,6 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Cat cat=new Cat();
+        cat.setName("Gio");
+        System.out.println(cat.getName());
     }
 }
 
