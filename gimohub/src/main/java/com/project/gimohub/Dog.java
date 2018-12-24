3e91ff447b5ab60407ea3e64d0cf6b31b5d4bd69
Nadeeshan Gimhana <gimhanadesilva.15@cse.mrt.ac.lk>
2018-12-24 18:44:09 +0530

    add Dog.java

---
+++ gimohub/src/main/java/com/project/gimohub/Dog.java
@@ -0,0 +1,36 @@
+package com.project.gimohub;
+
+/**
+ * Created by Dell on 12/24/2018.
+ */
+public class Dog {
+
+    private String name;
+    private String tail;
+    private String action;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTail() {
+        return tail;
+    }
+
+    public void setTail(String tail) {
+        this.tail = tail;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+}
