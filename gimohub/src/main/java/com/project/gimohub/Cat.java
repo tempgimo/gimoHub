99718054c01f754d329cbcfa786e00f861d1742d
Nadeeshan Gimhana <gimhanadesilva.15@cse.mrt.ac.lk>
2018-12-24 18:34:48 +0530

    add Cat.java

---
+++ gimohub/src/main/java/com/project/gimohub/Cat.java
@@ -0,0 +1,27 @@
+package com.project.gimohub;
+
+/**
+ * Created by Dell on 12/24/2018.
+ */
+public class Cat {
+
+    private String name;
+    private String tail;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTail() {
+        return tail;
+    }
+
+    public void setTail(String tail) {
+        this.tail = tail;
+    }
+}
