82db50c790c47f9bcc5275f0a35271450ab5e210
Nadeeshan Gimhana <gimhanadesilva.15@cse.mrt.ac.lk>
2018-12-24 18:53:19 +0530

    Add Fish.java

---
+++ gimohub/src/main/java/com/project/gimohub/Fish.java
@@ -0,0 +1,27 @@
+package com.project.gimohub;
+
+/**
+ * Created by Dell on 12/24/2018.
+ */
+public class Fish
+{
+    private String name;
+    private String breath;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreath() {
+        return breath;
+    }
+
+    public void setBreath(String breath) {
+        this.breath = breath;
+    }
+}
